a8e59a76a317a7bf1d8a6585a6f364c16ba5e945
Daniel Alexandro <danx_97@hotmail.com>
2020-06-11 23:54:09 -0500

    Add eslint rule

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import { Container } from "@material-ui/core";
 import NavBar from "../components/NavBar";
 
-function DefaultLayout(props: { children: React.ReactNode }) {
+function DefaultLayout(props: { children: React.ReactChild }) {
   return (
     <Fragment>
       <NavBar />
