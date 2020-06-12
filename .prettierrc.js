833e48f7d704af03a19ae5f2cb290fe8b0b289ef
Daniel Alexandro Martínez Chaverri <danx_97@hotmail.com>
2020-06-12 00:07:06 -0500

    Add Prettier rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   semi: true,
-  trailingComma: "all",
-  singleQuote: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  jsxSingleQuote: true,
   printWidth: 80,
-  tabWidth: 2
+  tabWidth: 2,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
 };
